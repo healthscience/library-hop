bb0d9a1a4a32ed39898d4f7ae3d91f71101a4c29
aboynejames <aboynejames@gmail.>
2025-02-14 20:37:41 +0000

    update to share flows

---
+++ src/account/peerNetwork.js
@@ -0,0 +1,119 @@
+'use strict'
+/**
+*  new, update, delete peers account network
+*
+*
+* @class PeerNetwork
+* @package    network-library
+* @copyright  Copyright (c) 2025 James Littlejohn
+* @license    http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+* @version    $Id$
+*/
+import util from 'util'
+import EventEmitter from 'events'
+
+class PeerNetwork extends EventEmitter {
+
+  constructor(Lib, Holepunch, Composer) {
+    super()
+    this.liveHolepunch = Holepunch
+    this.liveLib = Lib
+    this.libComposer = Composer
+  }
+
+  /**
+  * 
+  * @method 
+  *
+  */
+  getPeers = function (path, action, data) {
+
+  }
+
+  /**
+  * mange account queries
+  * @method accountManage
+  *
+  */
+  accountManage = async function (message) {
+    if (message.task.trim() === 'GET') {
+      // public or private library?
+      if (message.privacy === 'private') {
+        let cuesLib = await this.liveHolepunch.BeeData.getPeer(100)
+        // this.callbackCuesLib(message.data, cuesLib)
+      } else if (message.privacy === 'public') {
+        if (message.reftype === 'start-marker') {
+          // this.startCues()
+        } else {
+          // let publibCues = await this.+++(message.data)
+          // this.callback+++(publibCues)
+        }
+      }
+    } else if (message.task.trim() === 'PUT') {
+      if (message.privacy === 'private') { 
+        // save relationship
+        let saveContract = await this.savePeerProtocol(message.data)
+        let saveMessage = {}
+        saveMessage.type = 'account'
+        saveMessage.action = 'peer-new-relationship'
+        saveMessage.task = 'save-complete'
+        saveMessage.data = saveContract
+        // emits back to HOP library level - inform beebee saved
+        this.liveLib.emit('libmessage', JSON.stringify(saveMessage))
+        // if first time warm peer then complete that connection flow
+        this.liveLib.emit('complete-warmpeer', saveContract.data.key)
+      } else if (message.privacy === 'public') {
+        // need check if composer needed to form contract and then save
+
+      }
+    } else if (message.task.trim() === 'DEL') {
+      if (message.privacy === 'private') {
+        // private
+        let delFeedback = this.liveHolepunch.BeeData.deletePeer(message.data.key)
+      } else if (message.privacy === 'public') {
+        // public
+        let delFeedback = this.liveHolepunch.BeeData.deletePeer(message.data.key)
+      }
+    } else if (message.task.trim() === 'UPDATE') {
+      if (message.privacy === 'private') {
+        if (message.reftype === "new-peer-topic") {
+          // look up existing peer contract and add topic and make settop to true
+          let peerContract = await this.liveHolepunch.BeeData.getPeer(message.data.peerkey)
+          let peerPair = {}
+          peerPair.publickey = peerContract.key
+          peerPair.name = peerContract.value.name
+          peerPair.longterm = peerContract.value.longterm
+          peerPair.topic = message.data.topic
+          peerPair.setopic = true
+          peerPair.live = false
+          let updatePeer = await this.liveHolepunch.BeeData.savePeer(peerPair)
+          // need to inform beebee
+          updatePeer.value.live = true
+          this.liveLib.emit('complete-topic-save', updatePeer)
+          // need to infom, peer setting topic is live?
+        }
+      }
+    }
+  }
+
+  /**
+  * save a new peer to network
+  * @method savePeerProtocol
+  *
+  */
+  savePeerProtocol = async function (saveData) {
+    // let formedContract = this.libComposer.livePeer.peerPrepare(saveData)
+    // console.log(util.inspect(formedContract, {showHidden: false, depth: null}))
+    let saveContract = await this.liveHolepunch.BeeData.savePeer(saveData)
+    // format message for return
+    let saveMessage = {}
+    saveMessage.type = 'library'
+    saveMessage.action = 'peer-relationship'
+    saveMessage.task = 'save-complete'
+    saveMessage.data = saveContract
+    return saveMessage
+  }
+
+}
+
+export default PeerNetwork
